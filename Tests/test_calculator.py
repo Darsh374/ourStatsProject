cf316f93b771ed0ba8ad5c07fb5a0f0588202acc
jordanvz12 <jv337@njit.edu>
2020-03-10 15:06:14 -0400

    added test for multiplication

--- Tests/test_calculator.py
+++ Tests/test_calculator.py
@@ -17,5 +17,8 @@ class MyTestCase(unittest.TestCase):
         subtract = self.calculator.subtract(2 ,2)
         self.assertEqual(subtract, 0)
 
+    def test_multiplication(self):
+        product = self.calculator.product(3, 2)
+        self.assertEqual(product, 6)
 if __name__ == '__main__':
     unittest.main()
