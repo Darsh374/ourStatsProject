b1371d9349fb8d0c566851f51e8ac97b1845d1b0
jordanvz12 <jv337@njit.edu>
2020-03-12 18:14:55 -0400

    task: created a function that generates a random list and tested the function

--- RandomNums/RandomNums.py
+++ RandomNums/RandomNums.py
@@ -24,3 +24,14 @@ class RandomNums:
         random.seed(3)
         self.randomDec = random.random()
         return self.randomDec
+
+        # Generate a list of N random numbers with a seed and between a range of numbers - Both Integer and Decimal
+    def RangeNums(self):
+        random.seed(50)
+        randomNumbers = []
+        n = sum(randomNumbers)
+        i = 0
+        while i < n:
+            randomNumbers.append(random.randint(1, 100))
+            i += 1
+            pprint.pprint(randomNumbers)
