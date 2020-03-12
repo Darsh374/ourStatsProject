3abc958941899c0d9b9c42ad3cd3055c873336d9
jordanvz12 <jv337@njit.edu>
2020-03-12 09:06:25 -0400

    task: added a median function and tested

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,6 +1,6 @@
 from Calculator.Calculator import Calculator
 from Statistics.Mean import mean
-
+from Statistics.Median import median
 
 class Statistics(Calculator):
 
@@ -9,5 +9,10 @@ class Statistics(Calculator):
         self.data = None
 
     def mean(self, data):
-        self.result = mean()
+        self.result = mean(data)
         return self.result
+
+    def median(self, data):
+        self.result = median(data)
+        return self.result
+    
